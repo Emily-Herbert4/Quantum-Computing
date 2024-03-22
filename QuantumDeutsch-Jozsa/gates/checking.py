97f9a25b8dd6dccbc07c336a6211830e7a0da2c2
birdlord <herberte1@newpaltz.edu>
2024-03-22 12:36:39 -0400

    Changed uncerantiy of rounding errors to now 4 decimal places instead of 2

--- QuantumDeutsch-Jozsa/gates/checking.py
+++ QuantumDeutsch-Jozsa/gates/checking.py
@@ -7,9 +7,8 @@ one_sqrt2=(1/np.sqrt(2))
 #ket_cb = np.kron((1j/np.sqrt(2))*(g.ket_a), g.ket_H-g.ket_V)
 ket_c =(one_sqrt2)*np.array([0,0,-1j,1j])
 ket_b = (one_sqrt2)*np.array([1,-1,0,0])
-ket_test= 1.001*ket_c
-print(np.round(ket_test,3))
-print(np.round(ket_c,3))
+
+
 """ket_test= 1.0111*ket_b
 print(ket_test)
 print(np.round(ket_b,4))
@@ -17,7 +16,7 @@ print(np.round(ket_test,4))
 print(np.array_equiv(np.around(ket_b,4),np.around(ket_test,4)))"""
 def output(ket):
     #error 2 is around a 1.11% rounding error acounted for
-    error =2
+    error =4
     if np.array_equiv(np.round(ket,error),np.round(ket_c,error)) or np.array_equiv(np.round(ket,error),np.round(-ket_c,error)):
         return 'constant'
     if np.array_equiv(np.round(ket,error),np.round(ket_b,error)) or np.array_equiv(np.round(ket,error),np.round(-ket_b,error)):
