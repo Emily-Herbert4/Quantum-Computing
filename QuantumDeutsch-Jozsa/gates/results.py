d4cd1be6c9ef1a6025ed7a05ff6621a97b3709cf
birdlord <herberte1@newpaltz.edu>
2024-03-21 21:20:15 -0400

    The code works the opperators are not working ....grrr try other gates? dunno

---
+++ QuantumDeutsch-Jozsa/gates/results.py
@@ -0,0 +1,22 @@
+import gates as g
+import numpy as np
+
+one_sqrt2=(1j/np.sqrt(2))
+#Constant 
+#ket_cb = np.kron((1j/np.sqrt(2))*(g.ket_a), g.ket_H-g.ket_V)
+ket_c =one_sqrt2*np.array([0,0,-1j,1j])
+ket_b = one_sqrt2*np.array([1,-1,0,0])
+"""ket_test= 1.0111*ket_b
+print(ket_test)
+print(np.round(ket_b,4))
+print(np.round(ket_test,4))
+print(np.array_equiv(np.around(ket_b,4),np.around(ket_test,4)))"""
+def check(ket):
+    #error 2 is around a 1.11% rounding error acounted for
+    error =2
+    if np.array_equiv(np.round(ket,error),np.round(ket_c,error)) or np.array_equiv(np.round(ket,error),np.round(-ket_c,error)):
+        return 'constant'
+    if np.array_equiv(np.round(ket,error),np.round(ket_b,error)) or np.array_equiv(np.round(ket,error),np.round(-ket_b,error)):
+        return 'balance'
+    return "Does not fall under constant or balance function"
+#print(check(ket_test))
